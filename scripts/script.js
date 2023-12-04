94334af72406f9a90056a788b3a07d3246eff047
sfraga8357 <147834641+sfraga8357@users.noreply.github.com>
2023-12-03 19:05:16 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Stephanie Fraga
+    File Name: script.js
+    Date: 12-02-2023
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+	$('.hero').height($(window).height());
+});
